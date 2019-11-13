6d9a3fad73e0ea2845fea5fffed9b63b5b5c273f
Thomas Lacheze <thomas.lchz@gmail.com>
2019-11-13 16:26:48 +0100

    yelp ex done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'destroying all restaurants'
+Restaurant.destroy_all
+
+puts "creating 5 restaurants"
+restaurant1 = Restaurant.create!(
+  name: 'Le Galopin',
+  address: 'Paris',
+  category: 'french'
+)
+puts "created #{restaurant1.name}"
+
+restaurant2 = Restaurant.create!(
+  name: 'Cocolo',
+  address: 'Berlin',
+  category: 'japanese'
+)
+restaurant3 = Restaurant.create!(
+  name: 'Charlotte',
+  address: 'Berlin',
+  category: 'italian'
+)
+restaurant4 = Restaurant.create!(
+  name: 'Nanaya',
+  address: 'Paris',
+  category: 'japanese'
+)
+restaurant5 = Restaurant.create!(
+  name: 'East Mamma',
+  address: 'Paris',
+  category: 'italian'
+)
+
+puts "done!"
